58308fee8c92d73d50f69c369c8609f1924f917c
Matthew Emsak <104387212+matthew813709@users.noreply.github.com>
2024-07-31 20:22:23 -0400

    Update PyGame.py

--- design/PyGame.py
+++ design/PyGame.py
@@ -181,7 +181,7 @@ def main():
         register_email()
 
  # Add HomePage Link
-        st.markdown("[Homepage](https://www.maryville.edu/)", unsafe_allow_html=True)
+        st.markdown("[MaryVille University](https://www.maryville.edu/)", unsafe_allow_html=True)
     
 
 if __name__ == "__main__":
